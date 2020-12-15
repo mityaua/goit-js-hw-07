73766a03c2adf0dd0230fb3a93db543c349c6990
Dmytro <mityaua@gmail.com>
2020-12-15 20:02:43 +0200

    Рефакторинг 2й задачи

--- js/task-2.js
+++ js/task-2.js
@@ -7,9 +7,20 @@ const ingredients = [
   'Приправы',
 ];
 
-ingredients.map(element => {
-  const listItemRef = document.createElement('li');
-  listItemRef.textContent = element;
-  const ingredientsRef = document.querySelector('#ingredients');
-  return ingredientsRef.appendChild(listItemRef);
-});
+const createList = arr => {
+  arr.map(element => {
+    const listItemRef = document.createElement('li');
+    listItemRef.textContent = element;
+    const ingredientsRef = document.querySelector('#ingredients');
+    ingredientsRef.appendChild(listItemRef);
+    return ingredientsRef;
+  });
+};
+
+createList(ingredients);
+
+// 1. Методом map перебираем массив инградиентов;
+// 2. Создаем в переменной элемент списка;
+// 3. Добавляем для каждого элемента текст - как еденицу перебора массива;
+// 4. Записываем в переменную результат поиска нужного ID;
+// 5. Возвращаем в DOM создаваеммый элемент списка (с привязкой к нужному ID).
