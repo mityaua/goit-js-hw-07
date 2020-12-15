7de0f1b2e7a19410686a0f2de4c6458b90f2d60c
Dmytro <mityaua@gmail.com>
2020-12-15 16:24:05 +0200

    Инициализирует список задач

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const categoryRef = document.querySelectorAll('.item');
+
+console.log(`В списке ${categoryRef.length} категории.`);
+
+// const titleRef = document.querySelectorAll('h2');
+// console.log(`В списке ${titleRef.length} категории.`);
+
+// categoryRef.forEach(item => console.log(item));
