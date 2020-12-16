bb0df2a41ea4a3943b332410c666b0aa3b0ec02e
Dmytro <mityaua@gmail.com>
2020-12-16 09:55:14 +0200

    Рефакторит вторую задачу

--- js/task-2.js
+++ js/task-2.js
@@ -7,20 +7,14 @@ const ingredients = [
   'Приправы',
 ];
 
-const createList = arr => {
-  arr.map(element => {
-    const listItemRef = document.createElement('li');
-    listItemRef.textContent = element;
-    const ingredientsRef = document.querySelector('#ingredients');
-    ingredientsRef.appendChild(listItemRef);
-    return ingredientsRef;
-  });
+const createList = ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  return listItem;
 };
 
-createList(ingredients);
+const listItems = ingredients.map(ingredient => createList(ingredient));
 
-// 1. Методом map перебираем массив инградиентов;
-// 2. Создаем в переменной элемент списка;
-// 3. Добавляем для каждого элемента текст - как еденицу перебора массива;
-// 4. Записываем в переменную результат поиска нужного ID;
-// 5. Возвращаем в DOM создаваеммый элемент списка (с привязкой к нужному ID).
+const ingredientsRef = document.querySelector('#ingredients');
+
+ingredientsRef.append(...listItems);
--- js/task-3.js
+++ js/task-3.js
@@ -13,5 +13,22 @@ const images = [
     url:
       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
-  },
+    },
 ];
+
+const createGallery = obj =>
+  obj.map(picture => {
+    const item = document.createElement('li');
+    item.classList.add('js-gallery__link');
+    const image = document.createElement('img');
+    image.setAttribute('src', picture.url);
+    image.setAttribute('alt', picture.alt);
+    image.classList.add('js-gallery__img');
+    item.append(image);
+    const galleryRef = document.querySelector('#gallery');
+    galleryRef.appendChild(item);
+    galleryRef.classList.add('js-gallery');
+    return galleryRef;
+  });
+
+createGallery(images);
