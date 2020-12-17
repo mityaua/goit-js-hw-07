3b7bbe6ad123aa4d5b860357e854254cb9cc1078
Dmytro <mityaua@gmail.com>
2020-12-17 14:53:58 +0200

    Добавляет решени 5й задачи

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,33 @@
-const btnRef = document.querySelector('.item');
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
 
-btnRef.addEventListener('mouseenter', (event) => {
-    console.log(event.target);
-})
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+const counterRef = document.querySelector('#counter');
+const counterValueRef = counterRef.querySelector('#value');
+const decrementbtnRef = counterRef.querySelector('[data-action="decrement"]');
+const incrementbtnRef = counterRef.querySelector('[data-action="increment"]');
+
+let counterValue = 0;
+
+decrementbtnRef.addEventListener('click', decrement);
+incrementbtnRef.addEventListener('click', increment);
+
+function decrement() {
+  counterValue -= 1;
+  counterValueRef.textContent = counterValue;
+}
+
+function increment() {
+  counterValue += 1;
+  counterValueRef.textContent = counterValue;
+}
+
+// 1. Находим в DOM счётчик по ID
+// 2. Находим ID значения счётчика
+// 3. Находим кнопку + 1
+// 4. Находим кнопку - 1
+// 5. Создаем переменную, которая содержит текущее значение счётчика
+// 6. Добавляем на обе кнопки слушателя событий (при клике) и коллбек функцию
+// 7. Создаем две функции, которые изменяют значение переменной на 1 и записывают переменную в textContent значения счётчика.
---
+++ js/task-5.js
@@ -0,0 +1,13 @@
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
+
+const inputRef = document.querySelector('#name-input');
+const spanRef = document.querySelector('#name-output');
+
+inputRef.addEventListener(
+  'input',
+  event => (spanRef.textContent = event.target.value),
+);
+
+// 1. Находим в DOM поле ввода по ID
+// 2. Находим в DOM по ID текст, который будем менять
+// 3. Добавляем слушатель события на поле ввода, с событием input, параметром event и записываем значение события в textContent span.
