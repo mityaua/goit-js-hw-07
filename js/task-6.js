e3b4c964e72dccdbf657ade52ee2f7bf74afca89
Dmytro <mityaua@gmail.com>
2020-12-17 23:30:34 +0200

    Some fixes

--- js/task-6.js
+++ js/task-6.js
@@ -6,12 +6,13 @@
 const inputRef = document.querySelector('#validation-input');
 
 function checkinputLength(event) {
-    if (event.target.value.length === Number(inputRef.dataset.length)) {
-      inputRef.classList.add('valid');
-      inputRef.classList.remove('invalid');
-    } else {
-      inputRef.classList.add('invalid');
-    }
-};
+  if (event.target.value.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.add('valid');
+    inputRef.classList.remove('invalid');
+  } else {
+    inputRef.classList.add('invalid');
+    inputRef.classList.remove('valid');
+  }
+}
 
 inputRef.addEventListener('blur', checkinputLength);
